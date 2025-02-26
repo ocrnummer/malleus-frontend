7287572317af9c4093e033e37012d25710080411
ocrnummer <bergquist.alexander@gmail.com>
2025-02-26 09:45:26 +0100

    Move HomePage  to spicifik directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { HomePage } from './pages/HomePage/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
+      <HomePage />
     </>
   )
 }
