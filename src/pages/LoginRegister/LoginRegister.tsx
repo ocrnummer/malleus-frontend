6aefeff4cf202947fc1a323f5a3d9ee1f581d6bf
ocrnummer <bergquist.alexander@gmail.com>
2025-03-05 21:37:21 +0100

    feat: Setting up appwrite and adding the components necessary

---
+++ src/pages/LoginRegister/LoginRegister.tsx
@@ -0,0 +1,9 @@
+const LoginRegister = () => {
+    return ( 
+        <>
+            <div>LoginRegister</div>
+        </>
+     );
+}
+ 
+export default LoginRegister;
--- src/pages/SearchAds/SearchAds.tsx
+++ src/pages/SearchAds/SearchAds.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react';
+import db from '../../appwrite/databases.ts'
+import NewAdForm from '../../components/NewAdForm/NewAdForm';
+import { Models, Query } from 'appwrite';
+import AdListItem from './AdListItem/AdListItem.tsx';
+
+export interface AdType extends Models.Document {
+    title: string,
+    body: string
+}
+
 export const SearchAds = () => {
+    const [ads, setAds] = useState<AdType[]>([])
+
+    useEffect(() => {
+        init();
+    }, [])
+
+    const init = async () => {
+        const res = await db.ads.list(
+            [Query.orderDesc('$createdAt')]
+        );
+        setAds(res.documents)
+    }
+
     return (
         <>
-            <div>SearchAds</div>
+            <h2>SearchAds</h2>
+            <NewAdForm setAds={setAds}/>
+            <div>
+                {ads.map(
+                    (ad: AdType) => (
+                        <AdListItem key={ad.$id} setAds={setAds} adData={ad} />
+                    )
+                )}
+            </div>
             {/* Sökbox med filter */}
             {/* Lista med resultat */}
         </>
